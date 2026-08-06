fe93fcf929fec5542fa52f8aea116b65721a2715
Pradeep-database <pradeepkumaeresandb@gmail.com>
2026-08-06 21:40:02 +0530

    updated scripts

--- Day4/problem9.py
+++ Day4/problem9.py
@@ -6,11 +6,7 @@ df = pd.read_csv("employeees.csv")
 
 df["Salary Rank"] = df["Salary"].rank(ascending=False)
 
-print(df["Salary Rank"].sort_values())
-
 df.sort_values(by=["Salary Rank"],inplace=True)
-
-print(df)
 
 df.reset_index(inplace=True)
 
